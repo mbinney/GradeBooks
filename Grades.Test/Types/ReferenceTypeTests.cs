bd9ae50d48a0f73a754bcb69e0d35c2dfc665106
mbinney <mbinney@westfieldhealth.com>
2017-06-16 17:26:37 +0100

    Arrays section

--- Grades.Test/Types/ReferenceTypeTests.cs
+++ Grades.Test/Types/ReferenceTypeTests.cs
@@ -12,16 +12,47 @@ namespace Grades.Test.Types
     {
 
         [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {            
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "scott";
+            name = name.ToUpper();
+
+            Assert.AreEqual("SCOTT", name);
+        }
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(2015, 1, 1);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(2, date.Day);
+        }
+
+        [TestMethod]
         public void ValueTypesPassByValue()
         {
-            Int32 x = 46;
-            IncrementNumber(x);
-
-            Assert.AreEqual(46, x);            
+            int x = 46;
+            IncrementNumber(ref x);
+            Assert.AreEqual(47, x);            
 
         }
 
-        private void IncrementNumber(Int32 number)
+        private void IncrementNumber(ref int number)
         {
             number += 1;
         }
